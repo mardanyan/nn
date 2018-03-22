2661a12c3c8dda4939921fd576de5fd7a76b5b49
Tigran Mardanyan <mrde00@gmail.com>
2018-03-22 08:35:30 +0400

    changes

--- homework2/onelayer.py
+++ homework2/onelayer.py
@@ -3,23 +3,31 @@ import keras
 import numpy as np
 from matplotlib import pyplot as plt
 
-def showImage(img):
-
 
 class nn:
     def __init__(self):
-        (self.x_train, self.y_train), (self.y_test, self.y_test) = mnist.load_data()
-        self.x_train = self.x_train.reshape(60000, 784)
+        (self.x_train_orig, self.y_train), (self.x_test, self.y_test) = mnist.load_data()
+
+        print("x_train shape %s" % (self.x_train_orig.shape,))
+        print("x_test shape %s" % (self.x_test.shape,))
+
+        self.x_train = self.x_train_orig.reshape(60000, 784)
+        self.x_test = self.x_test.reshape(10000, 784)
+        self.x_train = self.x_train.astype('float32')
+        self.x_test = self.x_test.astype('float32')
+        self.x_train /= 255
+        self.x_test /= 255
+
         self.w = np.random.randn(10, 784)
+        print("w shape %s" % (self.w.shape,))
+        print("w row0 %s" % self.w[0, 0:10])
         self.alpha = 0.001
 
-
-    def showImage(self, img):
-        plt.imshow(img)
+    def showImage(self, index):
+        plt.imshow(self.x_train_orig[index])
         plt.colorbar()
         # plt.title('plot for image ")
         plt.show()
-
 
     def softmax(d):
         d = d - np.max(d)
@@ -27,12 +35,11 @@ class nn:
         summ = np.sum(exp)
         return exp / summ
 
-
     def dense(self, index):
         return self.w.dot(self.x_train[index])
 
-    def loss(self, softMax, index):
-        return -np.log(softMax, self.y_train[index])
+    # def loss(self, index):
+    #     return -np.log(self.y_train[index])
 
     def gradient(self, index, softMax):
         # los-i acancjal@ @st Y~
@@ -57,10 +64,10 @@ class nn:
             softMax = nn.softmax(denseValue)
 
             # calc loss
-            lossValue = self.loss(softMax, index)
+            # lossValue = self.loss(index)
 
             # loss's ajancjal @st w-i
-            gradientValue = self.grad(index, softMax)
+            gradientValue = self.gradient(index, softMax)
 
             self.update(gradientValue)
 
@@ -73,6 +80,7 @@ class nn:
 
 if __name__ == "__main__":
     n = nn()
+    # n.showImage(1)
     n.run()
 
 
